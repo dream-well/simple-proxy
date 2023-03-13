8387e847962a8e6d14df9046dfe87de410f47c14
dream-well <dreamwell506@outlook.com>
2023-03-13 21:30:32 +0900

    fix imports

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
+const { readFileSync } = require('fs');
 
 const app = express();
 
